9e44806118c580816b0ea99f7500beaa57b5ae25
BAKAY Erol <eba@cefic.be>
2020-07-09 14:41:33 +0200

    This commit is for Assignment1 - If Statements

---
+++ Assignment1.py
@@ -0,0 +1,6 @@
+name = input("What is your name?: ").title().strip()
+
+if name == "Joseph":
+    print(f"Hello {name}! The password is : W@12")
+else:
+    print("Hello, {}! See you later".format("Amina"))
